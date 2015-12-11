c032ae6fd0378756003c47b27a304aadc99d6e93
Nick Bunton <buntonn1@msu.edu>
2015-12-11 14:54:09 -0500

    ..
    ..

--- mi349final/main.js
+++ mi349final/main.js
@@ -1,35 +1,36 @@
 $(document).ready(function() {
+//when the header class is cliced
 $(".header").click(function() {
-
+  //grab that element
   $header = $(this);
-  //getting the next element
+  //get the next element
   $content = $header.next();
-  //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+  //display the next part
   $content.slideToggle(500, function() {
-    //execute this after slideToggle is done
-    //change text of header based on visibility of content div
+
     $header.text(function() {
-      //change text based on condition
-      //return $content.is(":visible") ? "Collapse" : "Expand";
+
     });
   });
 
 });
 });
 
+//this function makes the Twitter share work
+//this is all default code from Twitter developers website
 window.twttr = (function(d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0],
+  var js, fjs = d.getElementsByTagName(s)[0],
   t = window.twttr || {};
-if (d.getElementById(id)) return t;
-js = d.createElement(s);
-js.id = id;
-js.src = "https://platform.twitter.com/widgets.js";
-fjs.parentNode.insertBefore(js, fjs);
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
 
-t._e = [];
-t.ready = function(f) {
-  t._e.push(f);
-};
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
 
-return t;
-}(document, "script", "twitter-wjs"));
+  return t;
+  }(document, "script", "twitter-wjs"));
